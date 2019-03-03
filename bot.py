64852998b58b2a07c6cb5c555b65f8a3382efa51
connor <connormullett@gmail.com>
2019-03-03 12:07:00 -0500

    added help messages, added basic logging with clint and stdout, updated requirements.txt

--- bot.py
+++ bot.py
@@ -23,13 +23,13 @@ def on_ready():
     sys.stdout.write(f'{bot.user.id}\n')
 
 
-@bot.command()
+@bot.command(help='Run a test on the server')
 @asyncio.coroutine
 def test():
     yield from bot.say('Test SUXful!')
 
 
-@bot.command()
+@bot.command(help='Repeats message sent with command, used for testing messages to bot')
 @asyncio.coroutine
 def echo(*args):
     print(args)
@@ -41,16 +41,20 @@ def echo(*args):
             yield from bot.say('Test Successful')
 
 
-@bot.command(name='joke')
+@bot.command(name='joke', help='print a dad joke')
 @asyncio.coroutine
 def dad_joke():
+    sys.stdout.write(f'[{datetime.now()}] - Dad joke requested from' \
+            f'{bot.get_server}')
     headers = {'Accept': 'application/json'}
     url = 'https://icanhazdadjoke.com/'
     response = requests.get(url, headers=headers).json()
     if response['status'] == 200:
         yield from bot.say(response['joke'])
+        sys.stdout.write(f'[{datetime.now()}] - Dad joke sent to' \
+                f'{bot.get_server}')
     else:
-        print(f"JOKE COMMAND FAILED AT {datetime.now()} "\
+        sys.stdout.write(f"JOKE COMMAND FAILED AT {datetime.now()} "\
                 "STATUS CODE {response['status']}")
         yield from bot.say('I couldn\'t come up with a joke because ' \
                 'I\'m a bad bot :(')
